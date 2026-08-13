a72fde3fad84b16f87cc6ff752228246f880d38d
mcguinnlaura <mcguinnlaura@gmail.com>
2026-08-13 15:03:32 -0400

    Change heading size in contact section

--- src/components/section/contact-section.tsx
+++ src/components/section/contact-section.tsx
@@ -19,7 +19,7 @@ export default function ContactSection() {
         />
       </div>
       <div className="relative flex flex-col items-center gap-4 text-center">
-        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+        <h2 className="text-2xl font-bold tracking-tighter sm:text-4xl">
           {DATA.sections.contact.heading}
         </h2>
         <p className="mx-auto max-w-lg text-muted-foreground text-balance">
